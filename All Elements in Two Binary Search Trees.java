634d70ae2187886f8e1bd7d7ce87ab4a1d3b8c19
Jiaxiu Li <57015757+jiaxiuli@users.noreply.github.com>
2020-07-08 14:19:39 -0400

    Create All Elements in Two Binary Search Trees.java

---
+++ All Elements in Two Binary Search Trees.java
@@ -0,0 +1,61 @@
+/*
+Given two binary search trees root1 and root2.
+
+Return a list containing all the integers from both trees sorted in ascending order.
+
+ 
+
+Example 1:
+
+
+Input: root1 = [2,1,4], root2 = [1,0,3]
+Output: [0,1,1,2,3,4]
+Example 2:
+
+Input: root1 = [0,-10,10], root2 = [5,1,7,0,2]
+Output: [-10,0,0,1,2,5,7,10]
+Example 3:
+
+Input: root1 = [], root2 = [5,1,7,0,2]
+Output: [0,1,2,5,7]
+Example 4:
+
+Input: root1 = [0,-10,10], root2 = []
+Output: [-10,0,10]
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
+        List<Integer> list = new ArrayList<Integer>();
+        
+        dfs(root1, list);
+        dfs(root2, list);
+        Collections.sort(list);
+        return list;
+    }
+    
+    public TreeNode dfs(TreeNode root, List<Integer> list){
+        if( root == null ) return null;
+        
+        list.add(root.val);
+        
+        root.left = dfs(root.left, list);
+        root.right = dfs(root.right,list);
+        return root;
+    }
+}
