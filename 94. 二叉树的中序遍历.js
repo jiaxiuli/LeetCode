05ed25563f69820d9405c0e2dd1606f82e9eb226
Jiaxiu Li <57015757+jiaxiuli@users.noreply.github.com>
2021-04-25 17:24:23 +0800

    Create 94. 二叉树的中序遍历.js

---
+++ 94. 二叉树的中序遍历.js
@@ -0,0 +1,45 @@
+// 给定一个二叉树的根节点 root ，返回它的 中序 遍历。
+
+ 
+
+// 示例 1：
+
+
+// 输入：root = [1,null,2,3]
+// 输出：[1,3,2]
+// 示例 2：
+
+// 输入：root = []
+// 输出：[]
+// 示例 3：
+
+// 输入：root = [1]
+// 输出：[1]
+
+// 来源：力扣（LeetCode）
+// 链接：https://leetcode-cn.com/problems/binary-tree-inorder-traversal
+// 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。\
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function(root) {
+    var list = [];
+    list = bfs(root, list);
+    return list;
+};
+var bfs = function(root, list) {
+    if(root === null) return list;
+    bfs(root.left, list);
+    list.push(root.val);
+    bfs(root.right, list);
+    return list;
+}
