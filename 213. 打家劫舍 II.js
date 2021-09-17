4ca76e7e3a67c2ccd626ad7ef8d7afabe06d606e
Jiaxiu Li <57015757+jiaxiuli@users.noreply.github.com>
2021-09-17 10:14:31 +0800

    Create 213. 打家劫舍 II.js

---
+++ 213. 打家劫舍 II.js
@@ -0,0 +1,45 @@
+你是一个专业的小偷，计划偷窃沿街的房屋，每间房内都藏有一定的现金。这个地方所有的房屋都 围成一圈 ，这意味着第一个房屋和最后一个房屋是紧挨着的。同时，相邻的房屋装有相互连通的防盗系统，如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警 。
+
+给定一个代表每个房屋存放金额的非负整数数组，计算你 在不触动警报装置的情况下 ，今晚能够偷窃到的最高金额。
+
+ 
+
+示例 1：
+
+输入：nums = [2,3,2]
+输出：3
+解释：你不能先偷窃 1 号房屋（金额 = 2），然后偷窃 3 号房屋（金额 = 2）, 因为他们是相邻的。
+示例 2：
+
+输入：nums = [1,2,3,1]
+输出：4
+解释：你可以先偷窃 1 号房屋（金额 = 1），然后偷窃 3 号房屋（金额 = 3）。
+     偷窃到的最高金额 = 1 + 3 = 4 。
+示例 3：
+
+输入：nums = [0]
+输出：0
+
+f(i)表示以i为下标结尾的子数组的最大金额，所以有 f(i) = max{ f(i-2) + nums[i], f(i-1) }
+但是由于首位是连在一起的 所以分别计算 原数组的 0 ～ n-2, 1 ~ n-1 再取最大值就可以了
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var rob = function(nums) {
+    if (nums.length === 1) return nums[0];
+    if (nums.length === 2) return Math.max(nums[0], nums[1]);
+    const n = nums.length;
+    return Math.max(getRob(nums.slice(0, n-1)), getRob(nums.slice(1, n)));
+};
+function getRob(nums) {
+    const n = nums.length;
+    const dp = new Array(n);
+    dp[0] = nums[0];
+    dp[1] = Math.max(nums[0], nums[1]);
+    for ( let i = 2 ; i < n ; i++ ) {
+        dp[i] = Math.max(dp[i-2] + nums[i], dp[i-1]);
+    }
+    return dp[n-1];
+}
