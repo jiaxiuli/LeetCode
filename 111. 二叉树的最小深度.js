6b674a4606a5643d4b7e2f67137bccc545c0bd62
Jiaxiu Li <57015757+jiaxiuli@users.noreply.github.com>
2021-09-18 16:06:30 +0800

    Create 111. 二叉树的最小深度.js

---
+++ 111. 二叉树的最小深度.js
@@ -0,0 +1,47 @@
+给定一个二叉树，找出其最小深度。
+
+最小深度是从根节点到最近叶子节点的最短路径上的节点数量。
+
+说明：叶子节点是指没有子节点的节点。
+
+ 
+
+示例 1：
+
+
+输入：root = [3,9,20,null,null,15,7]
+输出：2
+示例 2：
+
+输入：root = [2,null,3,null,4,null,5,null,6]
+输出：5
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/minimum-depth-of-binary-tree
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var minDepth = function(root) {
+    if (root === null) return 0;
+    return dfs(root, 1);
+};
+var dfs = function(root, level) {
+    if (root === null) return Number.MAX_SAFE_INTEGER;
+    if (root.left === null && root.right === null) {
+        return level;
+    }
+    const levelLeft = dfs(root.left, level + 1);
+    const levelRight = dfs(root.right, level + 1);
+    return Math.min(levelLeft, levelRight);
+}
